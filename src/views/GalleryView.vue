febc82b078c1efa531074853ef713a3d4c0b17ee
jjlindsey <jlindsey010@gmail.com>
2026-01-20 16:10:05 -0800

    creating view

---
+++ src/views/GalleryView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="gallery">
+
+    </div>
+</template>
+<script setup>
+    import { ref, onMounted } from 'vue';
+    import {fetchArtworks} from '@/api/artworks';
+    import ArtworkCard from '@/components/ArtworkCard.vue';
+
+    const artworks = ref([]);
+
+    onMounted(async () => {
+        artworks.value = await fetchArtworks();
+    });
+</script>
+
+<style scoped>
+    .gallery {
+        display: flex;
+        gap: 16px;
+        padding: 20px;
+    }
+</style>
